bc85b894e8526f229cb5ed13946718eeaccc9207
Mingjie Liu <mr.liumingjie@gmail.com>
2023-03-07 20:31:49 +0800

    feat(http): implement some http utils

---
+++ falconlink/http/http_utils.cpp
@@ -0,0 +1,176 @@
+#include "http/http_utils.hpp"
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <filesystem>
+#include <sstream>
+#include <cstring>
+#include <cassert>
+#include <fstream>
+
+namespace falconlink {
+
+namespace http {
+
+
+Method toMethod(const std::string &method_str) {
+  auto method_str_formatted = format(method_str);
+  if (method_str_formatted == METHOD_TO_STRING.at(Method::GET)) {
+    return Method::GET;
+  }
+  if (method_str_formatted == METHOD_TO_STRING.at(Method::HEAD)) {
+    return Method::HEAD;
+  }
+  return Method::UNSUPPORTED;
+}
+
+Version toVersion(const std::string &version_str) {
+  auto version_str_formatted = format(version_str);
+  if (version_str_formatted == VERSION_TO_STRING.at(Version::HTTP_1_1)) {
+    return Version::HTTP_1_1;
+  }
+  return Version::UNSUPPORTED;
+}
+
+
+Extension toExtension(const std::string &extension_str) {
+  auto extension_str_formatted = format(extension_str);
+  if (extension_str_formatted == EXTENSION_TO_STRING.at(Extension::HTML)) {
+    return Extension::HTML;
+  }
+  if (extension_str_formatted == EXTENSION_TO_STRING.at(Extension::CSS)) {
+    return Extension::CSS;
+  }
+  if (extension_str_formatted == EXTENSION_TO_STRING.at(Extension::PNG)) {
+    return Extension::PNG;
+  }
+  if (extension_str_formatted == EXTENSION_TO_STRING.at(Extension::JPG)) {
+    return Extension::JPG;
+  }
+  if (extension_str_formatted == EXTENSION_TO_STRING.at(Extension::JPEG)) {
+    return Extension::JPEG;
+  }
+  if (extension_str_formatted == EXTENSION_TO_STRING.at(Extension::GIF)) {
+    return Extension::GIF;
+  }
+  return Extension::OCTET;
+}
+
+std::string ExtensionToMime(const Extension &extension)  {
+  if (extension == Extension::HTML) {
+    return MIME_HTML;
+  }
+  if (extension == Extension::CSS) {
+    return MIME_CSS;
+  }
+  if (extension == Extension::PNG) {
+    return MIME_PNG;
+  }
+  if (extension == Extension::JPG) {
+    return MIME_JPG;
+  }
+  if (extension == Extension::JPEG) {
+    return MIME_JPEG;
+  }
+  if (extension == Extension::GIF) {
+    return MIME_GIF;
+  }
+  if (extension == Extension::OCTET) {
+    return MIME_OCTET;
+  }
+  return MIME_OCTET;
+}
+
+std::vector<std::string>
+split(const std::string &str, const char *delim) {
+  std::vector<std::string> tokens;
+  if (str.empty()) {
+    return tokens;
+  }
+  std::string token;
+  size_t curr = 0;
+  size_t next;
+  size_t delim_len = strlen(delim);
+  while ((next = str.find(delim, curr)) != std::string::npos) {
+    tokens.emplace_back(str.substr(curr, next - curr));
+    curr = next + delim_len;
+  }
+  if (curr != str.size()) {
+    // one last word
+    tokens.emplace_back(str.substr(curr, str.size() - curr));
+  }
+  return tokens;
+}
+
+std::string join(const std::vector<std::string> &tokens, const char *delim) {
+  if (tokens.empty()) {
+    return {};
+  }
+  if (tokens.size() == 1) {
+    return tokens[0];
+  }
+  std::stringstream str_stream;
+  for (size_t i = 0; i < tokens.size() - 1; i++) {
+    str_stream << tokens[i] << delim;
+  }
+  str_stream << tokens[tokens.size() - 1];
+  return str_stream.str();
+}
+
+std::string trim(const std::string &str, const char *delim) {
+  size_t r_found = str.find_last_not_of(delim);
+  if (r_found == std::string::npos) {
+    return {};
+  }
+  size_t l_found = str.find_first_not_of(delim);
+  return str.substr(l_found, r_found - l_found + 1);
+}
+
+std::string toUpper(const std::string &str) {
+  std::string res;
+  std::transform(str.begin(), str.end(), res.begin(),
+                 [](char c) { return std::toupper(c); });
+  return res;
+}
+
+std::string format(const std::string &str) {
+  return toUpper(trim(str));
+}
+
+bool isDirectory(const std::string &directory_path) {
+  return std::filesystem::is_directory(directory_path);
+}
+
+bool  isCgiRequest(const std::string &resource_url) {
+  return resource_url.find(CGI_BIN) != std::string::npos;
+}
+
+bool isFileExists(const std::string &file_path) {
+  return std::filesystem::exists(file_path);
+}
+
+bool deleteFile(const std::string &file_path) {
+  return std::filesystem::remove(file_path);
+}
+
+size_t getFileSize(const std::string &file_path) {
+  assert(isFileExists(file_path));
+  return std::filesystem::file_size(file_path);
+}
+
+void loadFile(const std::string &file_path,
+              std::vector<unsigned char> &buffer) { // NOLINT
+  size_t file_size = getFileSize(file_path);
+  size_t buffer_old_size = buffer.size();
+  std::ifstream file(file_path);
+  buffer.resize(buffer_old_size + file_size);
+  assert(file.is_open());
+  file.read(reinterpret_cast<char *>(&buffer[buffer_old_size]),
+            static_cast<std::streamsize>(file_size));
+}
+
+
+}  // namespace http
+
+}  // namespace falconlink
---
+++ falconlink/include/http/http_utils.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+namespace falconlink {
+
+namespace http {
+
+static constexpr int READ_WRITE_PERMISSION = 0600;
+
+static constexpr char PARAMETER_SEPARATOR[] = {"&"};
+static constexpr char UNDERSCORE[] = {"_"};
+static constexpr char SPACE[] = {" "};
+static constexpr char DOT[] = {"."};
+static constexpr char CRLF[] = {"\r\n"};
+static constexpr char COLON[] = {":"};
+static constexpr char DEFAULT_ROUTE[] = {"index.html"};
+static constexpr char CGI_BIN[] = {"cgi-bin"};
+static constexpr char CGI_PREFIX[] = {"cgi_temp"};
+
+/* Common Header and Value */
+static constexpr char HEADER_SERVER[] = {"Server"};
+static constexpr char SERVER_TURTLE[] = {"Turtle/1.0"};
+static constexpr char HEADER_CONTENT_LENGTH[] = {"Content-Length"};
+static constexpr char HEADER_CONTENT_TYPE[] = {"Content-Type"};
+static constexpr char CONTENT_LENGTH_ZERO[] = {"0"};
+static constexpr char HEADER_CONNECTION[] = {"Connection"};
+static constexpr char CONNECTION_CLOSE[] = {"Close"};
+static constexpr char CONNECTION_KEEP_ALIVE[] = {"Keep-Alive"};
+static constexpr char HTTP_VERSION_TURTLE[] = {"HTTP/1.1"};
+
+/* MIME Types */
+static constexpr char MIME_HTML[] = {"text/html"};
+static constexpr char MIME_CSS[] = {"text/css"};
+static constexpr char MIME_PNG[] = {"image/png"};
+static constexpr char MIME_JPG[] = {"image/jpg"};
+static constexpr char MIME_JPEG[] = {"image/jpeg"};
+static constexpr char MIME_GIF[] = {"image/gif"};
+static constexpr char MIME_OCTET[] = {"application/octet-stream"};
+
+/* Response status */
+static constexpr char RESPONSE_OK[] = {"200 OK"};
+static constexpr char RESPONSE_BAD_REQUEST[] = {"400 Bad Request"};
+static constexpr char RESPONSE_NOT_FOUND[] = {"404 Not Found"};
+static constexpr char RESPONSE_SERVICE_UNAVAILABLE[] = {"503 Service Unavailable"};
+
+/* HTTP Method enum, only support GET/HEAD method now */
+enum class Method { GET, HEAD, UNSUPPORTED };
+
+/* HTTP version enum, only support HTTP 1.1 now */
+enum class Version { HTTP_1_1, UNSUPPORTED };
+
+/* Content Extension enum */
+enum class Extension { HTML, CSS, PNG, JPG, JPEG, GIF, OCTET };
+
+static const std::unordered_map<Method, std::string> METHOD_TO_STRING{
+    {Method::GET, "GET"},
+    {Method::HEAD, "HEAD"},
+    {Method::UNSUPPORTED, "UNSUPPORTED"}};
+
+static const std::unordered_map<Version, std::string> VERSION_TO_STRING{
+    {Version::HTTP_1_1, "HTTP/1.1"}, {Version::UNSUPPORTED, "UNSUPPORTED"}};
+
+static const std::unordered_map<Extension, std::string> EXTENSION_TO_STRING{
+    {Extension::HTML, "HTML"},  {Extension::CSS, "CSS"},
+    {Extension::PNG, "PNG"},    {Extension::JPG, "JPG"},
+    {Extension::JPEG, "JPEG"},  {Extension::GIF, "GIF"},
+    {Extension::OCTET, "OCTET"}};
+
+/* space and case insensitive */
+Method toMethod(const std::string &method_str);
+
+/* space and case insensitive */
+Version toVersion(const std::string &version_str);
+
+/* space and case insensitive */
+Extension toExtension(const std::string &extension_str);
+
+/* space and case insensitive */
+std::string ExtensionToMime(const Extension &extension);
+
+/**
+ * split a string into many sub strings, splitted by the specified delimiter
+ */
+std::vector<std::string> split(const std::string &str,
+                               const char *delim = SPACE);
+
+/**
+ * concatenate a collection of strings using the specified delimiter
+ */
+std::string join(const std::vector<std::string> &tokens,
+                 const char *delim = SPACE);
+
+/**
+ * Remove the leading and trailing specified delimiter (not inplace)
+ */
+std::string trim(const std::string &str, const char *delim = SPACE);
+
+/**
+ * Convert each character in a string to upper case (not inplace)
+ */
+std::string toUpper(const std::string &str);
+
+/**
+ * Apply Trim + ToUpper to a string and return the formatted version of it
+ */
+std::string format(const std::string &);
+
+/**
+ * Check if the path-specified directory exists
+ */
+bool isDirectory(const std::string &directory_path);
+
+/**
+ * Inspect if the request is dynamic CGI
+ * hardcode check if contains the cgi-bin folder in resource url path
+ */
+bool isCgiRequest(const std::string &resource_url);
+
+/**
+ * Check if the path-specified path exists
+ */
+bool isFileExists(const std::string &file_path);
+/**
+ * Delete a file if exists
+ */
+bool deleteFile(const std::string &file_path);
+
+/**
+ * Tell the size of a file in bytes.
+ * Assume this file already exists and is verified
+ */
+size_t getFileSize(const std::string &file_path);
+
+/**
+ * Load the file appending to be back of a vector of unsigned char
+ * able to contain binary data
+ */
+void loadFile(const std::string &file_path,
+              std::vector<unsigned char> &buffer);  // NOLINT
+
+}  // namespace http
+
+    
+} // namespace falconlink
